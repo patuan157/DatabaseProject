c20d2be23119460db184d795e14935068b925c01
Vince Dang <xuanvu001@e.ntu.edu.sg>
2017-10-10 14:35:05 +0800

    Draft #2 schema.json and query.sql

--- data/query.sql
+++ data/query.sql
@@ -1,5 +1,5 @@
 -- #1
-select publType, count(*) as count
+select publType, count(publType) as count
 from publication
 where year between 2000 and 2017
 group by publType;
@@ -11,7 +11,7 @@ where month = 'Jul' and id in (
   select c.id
   from publication p natural join conference c natural join link_conf_publ l
   group by c.id
-  having count(*) > 200;
+  having count(c.id) > 200;
 );
 
 -- #3
@@ -31,37 +31,120 @@ where p.id in (
 -- also publication year or proceedings year, for now assume that it's proceedings year
 
 -- c.
-select a.name
+select a.given_name || a.family_name as name
 from author a natural join publication p natural join link_author_publ l
 where p.id in (
   select p.id
   from publication p natural join conference c natural join link_conf_publ l
   where c.code = 'Z' and c.year = Y; -- assume that the conference name is short code and year is conference year
 )
-group by a.name
-having count(*) >= 2;
+group by a.given_name || a.family_name as name
+having count(a.id) >= 2;
 
 -- #4 (book and article currently not counted)
 -- a.
 select *
 from (
-  select a.name
+  select a.given_name || a.family_name as name
   from publication p natural join author a natural join link_author_publ l
   where p.id in (
     select p.id
     from publication p natural join conference c natural join link_conf_publ l
     where c.code = 'pvldb'
   )
-  group by a.name
-  having count(*) >= 10
+  group by a.given_name || a.family_name as name
+  having count(a.id) >= 10
 ) union (
-  select a.name
+  select a.given_name || a.family_name as name
   from publication p natural join author a natural join link_author_publ l
   where p.id in (
     select p.id
     from publication p natural join conference c natural join link_conf_publ l
     where c.code = 'sigmod'
   )
-  group by a.name
-  having count(*) >= 10
+  group by a.given_name || a.family_name as name
+  having count(a.id) >= 10
 );
+
+-- #4
+-- b.
+select *
+from (
+  select a.given_name || a.family_name as name
+  from publication p natural join author a natural join link_author_publ l
+  where p.id in (
+    select p.id
+    from publication p natural join conference c natural join link_conf_publ l
+    where c.code = 'pvldb'
+  )
+  group by a.given_name || a.family_name as name
+  having count(a.id) >= 15
+) union (
+  select a.given_name || a.family_name as name
+  from publication p natural join author a natural join link_author_publ l
+  where p.id in (
+    select p.id
+    from publication p natural join conference c natural join link_conf_publ l
+    where c.code = 'kdd'
+  )
+  group by a.given_name || a.family_name as name
+  having count(a.id) = 0
+);
+
+-- #5
+select p.decade, count(*)
+from publication p natural join conference c natural join link_conf_publ l
+where c.code = 'dblp'
+group by p.decade
+order by p.decade;
+
+-- #6
+select a.given_name || a.family_name as name
+from publication p natural join author a natural join link_author_publ l
+where p.id in (
+  select p.id
+  from publication p natural join conference c natural join link_conf_publ l
+  where c.title like '%data%' and p.noAuthors = MAX(p.noAuthors)
+);
+
+-- #7
+select a.given_name || a.family_name as name
+from publication p natural join author a natural join link_author_publ l
+where p.id in (
+  select p.id
+  from publication p natural join conference c natural join link_conf_publ l
+  where c.title like '%data%' and date_part('year', CURRENT_DATE) - p.year <= 5
+}
+group by a.given_name || a.family_name as name
+order by count(a.id) desc
+limit 10;
+
+-- #8
+select distinct c.title
+from publication p natural join conference c natural join link_conf_publ l
+where c.month = 'Jun'
+group by c.title, c.year, c.month
+having count(c.id) >= 100
+
+-- #9
+-- a.
+select a.given_name || a.family_name as name
+from author a 
+where a.family_name like 'H%' and 31 = (
+  select distinct count(p.year)
+  from publication p1 natural join author a1 natural join link_author_publ l1 
+  where a1.family_name = a.family_name and a1.given_name = a.given_name and date_part('year', CURRENT_DATE) - p.year <= 30
+  group by p.year
+)
+
+-- #9
+-- b.
+select a.given_name || a.family_name as name, count(*)
+from publication p natural join author a natural join link_author_publ l
+where a.id in (
+  select a.id
+  from publication p1 natural join author a1 natural join link_author_publ l1 
+  where p.year = MIN(p.year)
+)
+
+-- #10
